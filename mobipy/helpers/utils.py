de1fe1d89ba3e92f01d5291f4d2b7079e8a656d8
Pedro Maia <pedro.maia@ccc.ufcg.edu.br>
2019-11-05 16:38:49 -0300

    added new metrics + utils

--- mobipy/helpers/utils.py
+++ mobipy/helpers/utils.py
@@ -1,11 +1,46 @@
 import geopy as gp
 import pandas as pd
+import numpy as np
+import math
+from sklearn.cluster import DBSCAN
+
 
 def calculateDistance(lat1, lon1, lat2, lon2):
     coords1 = (lat1, lon1)
     coords2 = (lat2, lon2)
-    distance = gp.distance(coords1, coords2).m #utiliza o modelo WGS-84
+    distance = gp(coords1, coords2).m
     return distance
+
+
+def calculateMidPoint(dataframe, dataIdentifier):
+    x = 0
+    y = 0
+    z = 0
+
+    for row in dataframe.itertuples():
+        latitude = getattr(row, dataIdentifier.latitude) * (math.pi / 180)
+        longitude = getattr(row, dataIdentifier.longitude) * (math.pi / 180)
+
+        x += math.cos(latitude) * math.cos(longitude)
+        y += math.cos(latitude) * math.sin(longitude)
+        z += math.sin(latitude)
+
+    total = len(dataframe)
+
+    x = x / total
+    y = y / total
+    z = z / total
+
+    centralLongitude = math.atan2(y, x)
+    centralSquareRoot = math.sqrt(x * x + y * y)
+    centralLatitude = math.atan2(z, centralSquareRoot)
+
+    midPointLat = centralLatitude * (180 / math.pi)
+    midPointLon = centralLongitude * (180 / math.pi)
+
+    midPoint = (midPointLat, midPointLon)
+
+    return midPoint
 
 def get_time_spent_in_minutes(timestamp_start_time, timestamp_end_time):
     return int(timestamp_end_time-timestamp_start_time) / 60
@@ -14,3 +49,83 @@ def get_blox_plot_info(data):
     df = pd.DataFrame(data) 
     quantiles = df.quantile([0.25,0.5,0.75])
     return quantiles
+
+def cluster_points(points, max_radius, min_samples):
+    kms_per_radian = 6371.0088
+    epsilon = max_radius / kms_per_radian
+    db = DBSCAN(eps=epsilon, min_samples=min_samples, algorithm='ball_tree', metric='haversine').fit(np.radians(points))
+    cluster_labels = db.labels_
+    num_clusters = len(set(cluster_labels))
+    clusters = pd.Series([coords[cluster_labels == n] for n in range(num_clusters)])
+    print('Number of clusters: {}'.format(num_clusters))
+    return clusters
+
+def get_np_coords_from_df(dataframe, data_identifier):
+    coords = dataframe.as_matrix(columns=[data_identifier.lat_name, data_identifier.lon_name])
+    return coords
+
+def slice_geographic_data(dataframe, dataIdentifier, center_lat, center_lon, tolerance):
+    min_lats,max_lats,min_lons,max_lons = get_search_boundaries(center_lat, center_lon, tolerance)
+    return data[(data[dataIdentifier.lat_name] >= min_lats) & (data[dataIdentifier.lat_name] <= max_lats) &
+                (data[dataIdentifier.lon_name] >= min_lons) & (data[dataIdentifier.lon_name] <= max_lons)]
+
+def get_search_boundaries(center_lat, center_lon, 
+                          tolerance, threshold=1):
+    upper_limit = tolerance + threshold
+    lower_limit = tolerance - threshold
+    min_lats,max_lats,min_lons,max_lons = get_lat_long_with_tolerance(center_lat, 
+                                                                        center_lon, 
+                                                                        tolerance, 
+                                                                        upper_limit, 
+                                                                        lower_limit,
+                                                                        True)
+    distance = calculateDistance(min_lats, min_lons, center_lat, center_lon)
+    return round(min_lats, 6),round(max_lats, 6),round(min_lons, 6),round(max_lons, 6)
+    
+def get_lat_long_with_tolerance(center_lat, center_lon, 
+                                tolerance, 
+                                upper_limit, lower_limit, adjust=False):
+    km_per_degree = 111.32
+    meter_degree = (1 / km_per_degree) / 1000
+    coef = tolerance * meter_degree
+    min_lats = center_lat - coef
+    min_lons = center_lon - coef
+    max_lats = center_lat + coef
+    max_lons = center_lon + coef
+    distance = calculateDistance(max_lats, max_lons, center_lat, center_lon)
+    if(adjust == True):
+        if(distance > upper_limit):
+            new_tolerance = tolerance - ((distance - upper_limit) / 1.4)
+            return get_lat_long_with_tolerance(center_lat, center_lon, 
+                                           new_tolerance, 
+                                           upper_limit, lower_limit) 
+        if(distance < lower_limit):
+            new_tolerance = tolerance + ((lower_limit - distance) / 1.4)
+            return get_lat_long_with_tolerance(center_lat, center_lon, 
+                                           new_tolerance, 
+                                           upper_limit, lower_limit) 
+    else:
+        if(distance > upper_limit):
+            return get_lat_long_with_tolerance(center_lat, center_lon, 
+                                           tolerance - 1, 
+                                           upper_limit, lower_limit) 
+        if(distance < lower_limit):
+            return get_lat_long_with_tolerance(center_lat, center_lon, 
+                                           tolerance + 1, 
+                                           upper_limit, lower_limit) 
+    return min_lats, max_lats, min_lons, max_lons
+
+def get_closest_items(dataframe, dataIdentifier, center_lat, center_lon, 
+                distance_tolerance):
+    distances = {}
+    items_ids = []
+    if(len(dataframe) > 0):
+        for row in dataframe.itertuples():
+            distances[getattr(row, dataIdentifier.item_id)] = calculateDistance(center_lat, center_lon, 
+                         getattr(row, dataIdentifier.latitude), getattr(row, dataIdentifier.longitude))
+        ordered_distances = sorted(distances.items(), key=operator.itemgetter(1))
+        closest_distance = ordered_distances[0][1]
+        for item,distance in distances.items():
+            if(distance <= closest_distance + distance_tolerance):
+                items_ids.append(item)    
+    return items_ids
---
+++ mobipy/metrics/metrics.py
@@ -0,0 +1,48 @@
+import math
+from ..helpers import utils
+
+def radius_of_gyration(dataframe, dataIdentifier,  midPoint=None):
+    sum = 0
+    if midPoint is None:
+        midPoint = utils.calculateMidPoint(dataframe, dataIdentifier)
+    for row in dataframe.itertuples():
+        base = utils.calculateDistance(getattr(row, dataIdentifier.latitude), 
+            getattr(row, dataIdentifier.longitude), midPoint[0], midPoint[1])
+        sum += math.pow(base, 2)
+    sum = math.sqrt(sum / len(dataframe))
+    return sum
+
+def user_displacement_distance(dataframe, dataIdentifier):
+    sum_user_distances = 0
+    latitude_a = longitude_a = latitude_b = longitude_b = 0
+    for row in dataframe.itertuples():
+        if latitude_a == 0:
+            latitude_a = getattr(row, dataIdentifier.latitude)
+            longitude_a = getattr(row, dataIdentifier.longitude)
+        else:
+            if latitude_b == 0:
+                latitude_b = getattr(row, dataIdentifier.latitude)
+                longitude_b = getattr(row, dataIdentifier.longitude)    
+            sum_user_distances += utils.calculateDistance(latitude_a, longitude_a, latitude_b, longitude_b)
+            latitude_a = longitude_a = latitude_b = longitude_b = 0
+    return sum_user_distances
+
+def group_by_closeness(dataframe_a, dataframe_b, dataIdentifier, b_distance_tolerance=1, search_tolerance=50):
+    result_group = {}
+    for row in dataframe_a.itertuples():
+        center_lat = round(getattr(row, dataIdentifier.latitude), 6)
+        center_lon = round(getattr(row, dataIdentifier.longitude), 6)
+        a_id = getattr(row, dataIdentifier.item_id)
+        sliced_group_b = utils.slice_geographic_data(dataframe_b, dataIdentifier, center_lat, 
+                                               center_lon, search_tolerance)
+        for b_item in utils.get_closest_items(center_lat, center_lon, sliced_group_b, b_distance_tolerance):
+                time_spent_in_minutes = utils.get_time_spent_in_minutes(getattr(row, dataIdentifier.start_time), 
+                                                                  getattr(row, dataIdentifier.end_time))
+                if b_item in result_group:
+                        result_group[b_item][1].append(time_spent_in_minutes)
+                        result_group[b_item][2].append(a_id)
+                else:
+                        result_group[b_item] = [b_item, [time_spent_in_minutes], [a_id]]
+        for b_item in result_group:
+                result_group[b_item][1] = utils.get_blox_plot_info(result_group[b_item][1])
+        return result_group
